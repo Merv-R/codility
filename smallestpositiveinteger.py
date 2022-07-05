3012d2e52cff8f4eabd977de415c25ec936140b9
__ <35532152+Merv-R@users.noreply.github.com>
2022-07-04 20:19:07 -0400

    Updated the comment section

--- smallestpositiveinteger.py
+++ smallestpositiveinteger.py
@@ -10,10 +10,6 @@ Given A = [−1, −3], the function should return 1.
 Write an efficient algorithm for the following assumptions:
         N is an integer within the range [1..100,000];
         each element of array A is an integer within the range [−1,000,000..1,000,000].
-[1,3,6,4,1,2]
-[1,2,3]
-[-1,-3]
-[]
 """
 
 def solution(A):
